61b9e7a9d41dad92fc77697c4f00efbfea0297be
avinashluhana <aviluhana@gmail.com>
2022-04-04 23:50:20 +0500

    developing frontend of portfolio

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
