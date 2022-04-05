c50ab2a667bd39ad60038b304107a35bf0facc26
avinashluhana <aviluhana@gmail.com>
2022-04-06 00:46:24 +0500

    Arranged the code

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,26 +1,25 @@
 import React, { useEffect } from "react";
 import * as THREE from "three";
 
-
 const Home = () => {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.set(4, 4, 8);
 
-    useEffect (()=>{
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(
-          75,
-          window.innerWidth / window.innerHeight,
-          0.1,
-          1000
-        );
-        camera.position.set(4, 4, 8);
-
-        const canvas = document.querySelector(".homeCanvas");
-        const renderer = new THREE.WebGLRenderer({ canvas });
-
-    },[])
-  return <div className="home">
+    const canvas = document.querySelector(".homeCanvas");
+    const renderer = new THREE.WebGLRenderer({ canvas });
+  }, []);
+  return (
+    <div className="home">
       <canvas className="homeCanvas"></canvas>
-  </div>;
+    </div>
+  );
 };
 
 export default Home;
