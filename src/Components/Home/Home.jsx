a109590d0d6e91e34c329cf5a16cbe107fc9df99
avinashluhana <aviluhana@gmail.com>
2022-04-05 21:32:43 +0500

    added three.js code

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as THREE from "three";
+
 
 const Home = () => {
-  return <div></div>;
+
+    useEffect (()=>{
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+          75,
+          window.innerWidth / window.innerHeight,
+          0.1,
+          1000
+        );
+        camera.position.set(4, 4, 8);
+
+        const canvas = document.querySelector(".homeCanvas");
+        const renderer = new THREE.WebGLRenderer({ canvas });
+
+    },[])
+  return <div className="home">
+      <canvas className="homeCanvas"></canvas>
+  </div>;
 };
 
 export default Home;
