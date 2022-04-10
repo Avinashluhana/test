98cb73ea4d5476909ac6330b6dc6974a45d4f904
avinashluhana <aviluhana@gmail.com>
2022-04-10 22:33:17 +0500

    ended the route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element ={<Projects />} />
           <Route path="/projects" element ={<Projects />} />
-          <Route path="/projects" element ={<Projects />} />
 
 
         </Routes>
