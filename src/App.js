7be76652687ccdfbc9e9bf9c7c9ea781e645acf5
avinashluhana <aviluhana@gmail.com>
2022-04-10 22:48:08 +0500

    added three.js code with animation of moon and planet picture

--- src/App.js
+++ src/App.js
@@ -10,12 +10,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element ={<Projects />} />
-          <Route path="/projects" element ={<Projects />} />
-
-
         </Routes>
       </Router>
     </div>
