a67400abc4e6780c0ec949b298f985a157a22bb1
avinashluhana <aviluhana@gmail.com>
2022-04-14 22:05:03 +0500

    added page of projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import About from "./Components/About/About";
+import Projects from "./Components/Projects/Projects";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
         <Footer />
       </Router>
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,7 +10,11 @@ const About = ({ about }) => {
       </div>
       <div className="aboutContainer2">
         <div>
-          <img src="https://scontent.fkhi6-2.fna.fbcdn.net/v/t39.30808-1/269571240_4649984255098519_6041326465643828892_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=QYQFHnNweG4AX-U2D2l&_nc_ht=scontent.fkhi6-2.fna&oh=00_AT-KCXh2oNw8bo0z_io49vncXUlexvAtQvui0Etch4fsAA&oe=625D58D7" alt="Abhi" className="aboutAvatar" />
+          <img
+            src="https://scontent.fkhi6-2.fna.fbcdn.net/v/t39.30808-1/269571240_4649984255098519_6041326465643828892_n.jpg?stp=dst-jpg_p240x240&_nc_cat=107&ccb=1-5&_nc_sid=7206a8&_nc_ohc=QYQFHnNweG4AX-U2D2l&_nc_ht=scontent.fkhi6-2.fna&oh=00_AT-KCXh2oNw8bo0z_io49vncXUlexvAtQvui0Etch4fsAA&oe=625D58D7"
+            alt="Abhi"
+            className="aboutAvatar"
+          />
 
           <Typography
             variant="h4"
